178c0c3b59b0c4dd4f3981b24d94566bebd20a94
Scotter17 <sensonticcharlesscott@gmail.com>
2025-07-30 16:57:02 -0700

    Update act2.java

--- src/scottd/act2.java
+++ src/scottd/act2.java
@@ -1,9 +1,25 @@
-package scottd;
+package act1panilag;
+
+import java.util.Scanner;
 
 public class act2 {
     public static void main(String[] args) {
-        System.out.println("Activity 2");
-        
-    }
-    
-}
+        try (Scanner sc = new Scanner(System.in)) {
+            int num;
+            
+            do {
+                System.out.print("Enter a number: ");
+                num = sc.nextInt();
+                
+                if (num == 0) {
+                    System.out.println("Inputted number is Zero");
+                } else if (num % 2 != 0) {
+                    System.out.println("Inputted number is Odd");
+                } else {
+                    System.out.println("Inputted number is Even");
+                }
+                
+            } while (num != 0);
+            
+            System.out.println("End");
+        }
